093f4b907925eb3851d2d3a4ec6b65ba1b58bbe9
Thomas Pabst <thomas.pabst@gmail.com>
2014-04-24 01:25:32 +0200

    add Formule calcul des intêrets & tableau amortissement

--- fonction_finance.py
+++ fonction_finance.py
@@ -1,7 +1,7 @@
-# Methodes et formaules de de calculs de finances
+# Methodes et formules de de calculs de finances
+# -*-coding:Utf-8 -*
 
-
-# Formaule Calcul de la Mensualité
+# Formule Calcul de la Mensualité
 #
 # C : montant emprunté en euros
 # m : mensualité en euros
@@ -14,10 +14,9 @@
 #                             12
 # Formule :  m = ---------------------------
 #                       /       t  \ -42
-#                  1 - | 1 + ----  | 
+#                  1 - | 1 + ----  |
 #                       \      12  /
 #
-
 
 def calculMensualite(C, n, t):
     return (float(C) * float(t) / 12) / (1 - (1 + float(t) / 12)**(float(-n)))
@@ -38,3 +37,33 @@ def calculTauxPeriodique(txAn, echAn):
     return float(txAn)/float(echAn)
 
 
+
+# Formule calcul des intêrets
+
+def calculInteret(capitalRestant, tauxAnnuel, echParAn):
+    tauxPeriodique = calculTauxPeriodique(tauxAnnuel, echParAn)
+    return tauxPeriodique * capitalRestant
+
+
+
+# tableau amortissement
+
+def amortissement(capital, tauxAnnuel, nbrEch, echParAn):
+    if nbrEch > 0:
+        mensualite = calculMensualite(capital, nbrEch, tauxAnnuel)
+        interet = calculInteret(capital, tauxAnnuel, echParAn)
+        capitalRbst = mensualite - interet
+        newCapital = capital - capitalRbst
+        print("%5.2f" % capital, "%.2f" % mensualite, "%.2f" % interet, "%.2f" % capitalRbst, nbrEch)
+        amortissement(newCapital, tauxAnnuel, nbrEch -1, echParAn)
+
+
+
+# tableau amortissement epargne
+def epargne(a,b):
+    x=0
+    while x < 6:
+        y = a * x + b
+        print x+7
+        print y
+        x=x+1
